552a3ccfc8ac8852731db8198de1a459885a527e
thekingrenz23 <thekingrenz23@gmail.com>
2022-05-08 00:20:04 +0800

    first commit

--- app/src/main/java/com/example/googlemapsbottomnav/MainActivity.kt
+++ app/src/main/java/com/example/googlemapsbottomnav/MainActivity.kt
@@ -22,6 +22,9 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
+/**
+ * Sample by RENZ CARLO SALANGA
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
